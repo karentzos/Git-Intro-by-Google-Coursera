fe6e544b191d5d3b397441347ef97ca82a58a7de
ekarentz <efstathios.karentzos@cern.ch>
2020-10-19 23:20:26 +0200

    correct the error of the unpopulated submodule script. Use git rm --cached . -rf inside the problematic dir.

---
+++ scripts/all_checks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def check_reboot():
+        """ Returns TRUE if the computer has a pending reboot."""
+        return os.path.exists("/run/reboot-required")
+
+def main():
+        #pass
+        if check_reboot():
+                print("Pending Reboot.")
+                sys.exit(1)
+        print("Everything is ok!")
+        sys.exit(0)
+
+main()
+
+
